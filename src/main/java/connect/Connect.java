938bde25de408fb3820ac95c1029d788cd2a39fe
Sergio <sdepineresramirez@utb.edu.co>
2022-11-23 10:57:25 -0500

    Fixed database connection problem

--- src/main/java/connect/Connect.java
+++ src/main/java/connect/Connect.java
@@ -6,18 +6,18 @@ import java.sql.DriverManager;
 
 public class Connect { 
     public Connection connect(){ 
-        String url= "jdbc:sqlite:db1.db";
+        String url= "jdbc:sqlite:src/main/resources/db1.db";
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url);
-            System .out .println ("Connection to SQlite has been stablished");
+            System.out.println("Connection to SQlite has been stablished");
         }
         catch (SQLException e){
-            Alert alerta = new Alert (Alert .AlertType .ERROR); //JOptionPane.showMessageDialog (null,"Error: "+e.getMessage()); alerta .setHeaderText (null);
-            alerta .setTitle ("Error");
-            alerta .setContentText ( e .getMessage());
-            alerta .show();
-            System .out .println ( e .getMessage());
+            Alert alerta = new Alert(Alert.AlertType.ERROR); //JOptionPane.showMessageDialog (null,"Error: "+e.getMessage()); alerta .setHeaderText (null);
+            alerta.setTitle("Error");
+            alerta.setContentText(e.getMessage());
+            alerta.show();
+            System.out.println(e.getMessage());
         }
         return conn ;
     }
