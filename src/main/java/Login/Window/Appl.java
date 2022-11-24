87c868af5e9b8e773402bb1f5dc1e9b0a9a1e4f0
Sergio <sdepineresramirez@utb.edu.co>
2022-11-24 10:57:19 -0500

    changed gender field to selection, added show button

--- src/main/java/Login/Window/Appl.java
+++ src/main/java/Login/Window/Appl.java
@@ -1,11 +1,16 @@
 package Login.Window;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import java.util.Vector;
+import connect.Query;
 
 public class Appl {
 
@@ -14,6 +19,9 @@ public class Appl {
 
     @FXML
     private Button ClearButton;
+
+    @FXML
+    private Button ShowButton;
 
     @FXML
     private TextArea Output;
@@ -25,7 +33,7 @@ public class Appl {
     private TextField gche;
 
     @FXML
-    private TextField gender;
+    private ChoiceBox<String> gender;
 
     @FXML
     private TextField ginf;
@@ -40,7 +48,35 @@ public class Appl {
     private TextField name;
 
     @FXML
+    protected void initialize() {
+        Show(null);
+        ObservableList<String> choices = FXCollections.observableArrayList("M","F");
+        gender.setItems(choices);
+    }
+
+    @FXML
     void Add(ActionEvent event) {
+        
+    }
+
+    @FXML
+    void Show(ActionEvent event) {
+        String us = Query.request("students");
+        String[] temp = us.split("\n");
+        Vector<String> ids = new Vector<String>();
+        Vector<String> names = new Vector<String>();
+        Vector<String> genders = new Vector<String>();
+        Vector<String> ginfs = new Vector<String>();
+        Vector<String> gphyss = new Vector<String>();
+        Vector<String> gches = new Vector<String>();
+        for (int i = 0; i < temp.length; i++) {
+            if (i%6 == 0) ids.add(temp[i].replace("id: ", ""));
+            if ((i-1)%6 == 0) names.add(temp[i].replace("name: ", ""));
+            if ((i-2)%6 == 0) genders.add(temp[i].replace("gender: ", ""));
+            if ((i-3)%6 == 0) ginfs.add(temp[i].replace("ginf: ", ""));
+            if ((i-4)%6 == 0) gphyss.add(temp[i].replace("gphys: ", ""));
+            if ((i-5)%6 == 0) gches.add(temp[i].replace("gche: ", ""));
+        }
         
     }
 
@@ -48,7 +84,7 @@ public class Appl {
     void Clear(ActionEvent event) {
         id.setText(null);
         name.setText(null);
-        gender.setText(null);
+        
         ginf.setText(null);
         gphys.setText(null);
         gche.setText(null);
