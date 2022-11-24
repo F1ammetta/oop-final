a019f1b78dc24aef624181198f8921c585b33c3f
Sergio <sdepineresramirez@utb.edu.co>
2022-11-23 20:51:20 -0500

    finnished query methods

--- src/main/java/connect/Query.java
+++ src/main/java/connect/Query.java
@@ -1,6 +1,5 @@
-package Login.Window;
+package connect;
 import java.sql.Connection;
-import connect.Connect;
 import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
@@ -27,8 +26,18 @@ public class Query {
         return obj;
     }
 
-    public static void insert(){
-        String query = "INSERT INTO Tabla(campo1,campo2… ,campon) VALUES("+valorCampo1+","+"'"+valorCampo2+","+valorCampoInt+");";
+    public static void insert(String username, String password){
+        String query = "INSERT INTO users(username, passwd) VALUES('"+username+"','"+password+"');";
+        Connect ocon = new Connect();
+        try (Connection con = ocon.connect(); Statement st = con.createStatement()){
+            st.executeUpdate(query);
+        }catch (SQLException e) {
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+
+    public static void insert(int id, String name, char gender, float inf, float phys, float che){
+        String query = "INSERT INTO students(id, name, gender, ginf, gphys, gche) VALUES("+id+",'"+name+"','"+gender+"',"+inf+","+phys+","+che+");";
         Connect ocon = new Connect();
         try (Connection con = ocon.connect(); Statement st = con.createStatement()){
             st.executeUpdate(query);
